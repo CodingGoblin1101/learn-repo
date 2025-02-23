8785ba091dc1a2c1f6f46de0056e160bc594ec2c
FeyEtros <134168682+FeyEtros@users.noreply.github.com>
2025-02-23 23:03:50 +0100

    strategy pattern implemented with duck example

--- strategy_pattern.py
+++ strategy_pattern.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 
 
-class MakeDuck():
+class MakeDuck:
 
     def __init__(self, behavior):
         self._behavior = behavior
