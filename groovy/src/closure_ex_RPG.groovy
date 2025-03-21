dadfbc0812e21b9e4c60d3fa0b8b013695c1f2cd
FeyEtros <134168682+FeyEtros@users.noreply.github.com>
2025-03-21 13:32:50 +0100

    usage of closure to manipulate property of an object

--- groovy/src/closure_ex_RPG.groovy
+++ groovy/src/closure_ex_RPG.groovy
@@ -6,6 +6,7 @@ class CharacterGen {
     int HP
     int ATK
     int DEF
+    int EXP
     String name
 
     def set_status() {
@@ -60,8 +61,9 @@ class WeaponGen{
 
 }
 
+"""set Status with User input"""
 CharacterGen CharA = new CharacterGen()
-CharA.setName("revy")
+CharA.setName("Revy")
 //CharA.set_status()
 CharA.ATK = 50
 println CharA
@@ -73,11 +75,60 @@ println CharB
 WeaponGen Sword = new WeaponGen()
 Sword.ATK = 100
 
+"""ATK buff usable with both character & weapon"""
 //delegate in closure
-def ATK_buff = { -> delegate.ATK * 1.5}
+def ATK_buff = { -> delegate.ATK * 2}
 ATK_buff.delegate = Sword
 println "ATK Sword with buff: $ATK_buff"
 println "flat Sword ATK: $Sword.ATK"
 ATK_buff.delegate = CharA
 println "ATK Char with buff: $ATK_buff"
 println "flat Char ATK: $CharA.ATK"
+
+def ATK_buff_end = { -> delegate.ATK / 2}
+
+CharacterGen EnemyA = new CharacterGen()
+EnemyA.DEF = 50
+EnemyA.HP = 500
+EnemyA.name = "Goblin"
+EnemyA.EXP = 60
+
+
+"""damage calculation:
+((Char.ATK * 1.5 + Wpn.ATK * 0.5) - Enemy.DEF) * (1 + RNG_factor)
+"""
+def Attack_Enemy(Object Char, Object Wpn, Object Enemy){
+    RNG_factor = new Random().nextFloat(0.3)
+
+    Double DMG = ((Char.ATK * 1.5 + Wpn.ATK * 0.5) - Enemy.DEF) * (1 + RNG_factor)
+    DMG = DMG.round(0)
+    Enemy.HP -= DMG
+    println "$Char.name did $DMG DMG to $Enemy.name"
+
+    if(Enemy.HP <= 0){
+        println "$Enemy.name was slain. You WON! $char.name gained $Enemy.EXP exp"
+        Char.EXP += Enemy.EXP
+
+    }
+}
+
+
+println ""
+println ""
+println "~~~Fight starts~~~"
+CharA.ATK = ATK_buff()
+Attack_Enemy(CharA, Sword, EnemyA)
+println EnemyA.HP
+
+
+Attack_Enemy(CharA, Sword, EnemyA)
+println EnemyA.HP
+
+ATK_buff_end.delegate = CharA
+CharA.ATK = ATK_buff_end()
+
+Attack_Enemy(CharA, Sword, EnemyA)
+println EnemyA.HP
+
+Attack_Enemy(CharA, Sword, EnemyA)
+println CharA
